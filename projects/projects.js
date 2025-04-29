0b594d323acb4af806caded7cc057e11d9b596d2
Ryan Lindberg <lindbergryan04@gmail.com>
2025-04-28 19:33:26 -0700

    add basic pie chart to projects page

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 const projects = await fetchJSON('../lib/projects.json');
 
@@ -13,3 +14,27 @@ const projectsAmount = projectArticles.length;
 if (projectTitle) {
   projectTitle.textContent = `${projectsAmount} Projects`;
 }
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let data = [1, 2];
+let total = d3.sum(data);
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+let colors = ['gold', 'purple'];
+
+arcs.forEach((arc, i) => {
+    d3.select('svg')
+    .append('path')
+    .attr('d',arc)
+    .attr('fill', colors[i]);
+  });
+  
